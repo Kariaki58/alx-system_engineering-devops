3994b44c29f6f53a11253e2edaba86445e39d43c
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-27 01:06:47 +0100

    puppet work ooh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,13 @@
-file { "append to puppet":
-  path    => "/home/mi4/.ssh/config",
-  ensure  => present,
-  content => "Host ubuntuserver\n\tHostname 107.23.113.143\n\tUser ubuntu\n\tPasswordAuthentication no\n",
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => "/etc/ssh/ssh_config",
+  line   => "\tPasswordAuthentication no",
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '\tIdentityFile ~/.ssh/school',
 }
