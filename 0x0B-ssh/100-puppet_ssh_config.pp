dbaa4d37d29f17279745aa25bd641d2b2281b415
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-27 01:08:03 +0100

    puppet work ooh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,17 @@
+# manifest testing
+
 include stdlib
 
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => "/etc/ssh/ssh_config",
-  line   => "\tPasswordAuthentication no",
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
+file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '\tIdentityFile ~/.ssh/school',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
