0030c4b90ef919211896e9e4109436754844b486
Kariaki58 <programmerjavascript901@gmail.com>
2023-11-27 01:08:48 +0100

    using tab

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,13 +5,13 @@ include stdlib
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+  line   => '\tPasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Delare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '     IdentityFile ~/.ssh/school',
+  line   => '\tIdentityFile ~/.ssh/school',
   replace => true,
 }
