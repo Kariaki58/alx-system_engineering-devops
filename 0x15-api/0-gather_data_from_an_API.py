967e21e80ee5c60e5f8fac7e82aa6a1d9315343f
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-02 23:52:28 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""gather data from an api"""
 import requests
 import sys
 
-
 def get_employee_todo_progress(employee_id):
-    """get employee todo"""
     base_url = "https://jsonplaceholder.typicode.com"
-
+    
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
-
+    
     todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
     todo_data = todo_response.json()
-
+    
     completed_tasks = [task['title'] for task in todo_data if task['completed']]
-
-    print(
-            f"Employee {user_data['name']}\
-                    is done with\
-                    tasks({len(completed_tasks)}/{len(todo_data)}):"
-            )
+    
+    print(f"Employee {user_data['name']}\
+            is done with\
+            tasks({len(completed_tasks)}/{len(todo_data)}):")
     for task in completed_tasks:
         print(f"\t{task}")
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
