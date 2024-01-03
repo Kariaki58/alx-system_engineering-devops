9209aab9aa9b21842c3a19b3afaa81b6e48bea0c
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 11:09:42 +0100

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -11,15 +11,13 @@ def main(data):
     users = requests.get(base_url + "users/{}".format(data)).json()
     user_name = users.get("username")
     todo = requests.get(base_url + "todos", params={"userId": data}).json()
-     
     with open("{}.json".format(data), "w") as file_json:
         json.dump({data: [{"task": d.get("title"),
-                            "completed": d.get("completed"),
-                            "username": user_name} for d in todo]},
+                           "completed": d.get("completed"),
+                           "username": user_name} for d in todo]},
                   file_json)
 
 
 if __name__ == "__main__":
     input_data = sys.argv[1]
     main(input_data)
-
