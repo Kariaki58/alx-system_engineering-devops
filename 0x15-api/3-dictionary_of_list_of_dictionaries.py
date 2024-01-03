4c4ac0246a625f2b1c3ca00d4a02e5701e30938f
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 11:17:26 +0100

    dictionary of list of dictionary

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import requests
+import json
+
+
+if __name__ == "__main__":
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    all_employees_tasks = {}
+
+    for user_id in range(1, 11):
+        todo = requests.get(base_url + "todos", params={"userId": user_id}).json()
+        
+        username = requests.get(base_url + "users", params={"id": user_id}).json()[0]["username"]
+        user_tasks = []
+
+        for task in todo:
+            task_details = {
+                "username": username,
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            user_tasks.append(task_details)
+
+        all_employees_tasks[str(user_id)] = user_tasks
+
+    output_file = 'todo_all_employees.json'
+    with open(output_file, 'w') as json_file:
+        json.dump(all_employees_tasks, json_file)
