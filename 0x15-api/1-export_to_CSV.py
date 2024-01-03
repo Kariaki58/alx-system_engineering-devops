9cf069701d519ea687e7a18b58e863ac05e81f90
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 10:35:27 +0100

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-"""
-export to csv
-"""
-
 import csv
 import json
 import requests
-from sys import argv
+import sys
+
+
+def main(data):
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users/{}".format(data)).json()
+    user_name = users.get("username")
+    todo = requests.get(base_url + "todos", params={"userId": data}).json()
+
+    with open("{}.csv".format(data), mode="w") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            writer.writerow([data, task['userId'],
+                             task['completed',
+                            task['title']]])
 
 
 if __name__ == "__main__":
-
-    id = argv[1]
-    todo_id = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
-    userid = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-
-    response = requests.Session()
-
-    todos = response.get(todo_id)
-    user = response.get(userid)
-
-    response_json = todos.json()
-    user_json = user.json()['username']
-
-    content = 0
-
-    for data in response_json:
-        if data['completed']:
-            content += 1
-
-    fileCSV = id + '.csv'
-
-    with open(fileCSV, "w", newline='') as csvfile:
-        write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
-        for i in response_json:
-            write.writerow([id, user_json, i.get('completed'), i.get('title')])
+    input_data = sys.argv[1]
+    main(input_data)
