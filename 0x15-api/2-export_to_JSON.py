d1886f3b88e6691962e42bc2fd725a8bdced0406
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 10:50:00 +0100

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,32 +5,19 @@ import sys
 import requests
 
 
+def main(data):
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users/{}".format(data)).json()
+    user_name = users.get("username")
+    todo = requests.get(base_url + "todos", params={"userId": data}).json()
+
+    filename = data + ".json"
+    with open(filename, mode="w") as file:
+        json.dump({data: [{"task": d.get("title"),
+                           "completed": d.get("completed"),
+                           "username": user_name} for d in todo]}, file)
+
 if __name__ == "__main__":
-    sessionReq = requests.Session()
-
-    id = sys.argv[1]
-    todoid = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
-    username = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
-
-    getter = sessionReq.get(todoid)
-    getteruser = sessionReq.get(username)
-
-    json_req = getter.json()
-    usr = getteruser.json()['username']
-
-    taskData = []
-    serizlided = {}
-
-    for all_Emp in json_req:
-        taskData.append(
-            {
-                "task": all_Emp.get('title'),
-                "completed": all_Emp.get('completed'),
-                "username": usr,
-            })
-    serizlided[id] = taskData
-
-    file_Json = id + ".json"
-    with open(file_Json, mode='w') as f:
-        json.dump(serizlided, f)
+    input_data = sys.argv[1]
+    main(input_data)
 
