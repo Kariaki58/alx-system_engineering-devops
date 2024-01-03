ce1b6eb2529d930dc67fe423d3ff69ee9be386ad
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-03 03:29:10 +0100

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+export to csv
+"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    sessionReq = requests.Session()
+
+    id = argv[1]
+    todo_id = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id)
+    userid = 'https://jsonplaceholder.typicode.com/users/{}'.format(id)
+
+    employee = sessionReq.get(todo_id)
+    employeeName = sessionReq.get(userid)
+
+    response_json = employee.json()
+    user_json = employeeName.json()['username']
+
+    totalTasks = 0
+
+    for done_tasks in response_json:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    fileCSV = id + '.csv'
+
+    with open(fileCSV, "w", newline='') as csvfile:
+        write = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        for i in response_json:
+            write.writerow([id, user_json, i.get('completed'), i.get('title')])
