34d8a4aa833d0ebdb420121c60b3db3d569c4cf2
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-16 16:25:06 +0100

    just trying this

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 """
-check how many subsribers
+Check the number of subscribers for a subreddit using the Reddit API.
 """
-import requests
 
+import requests
 
 def number_of_subscribers(subreddit):
     """
-    number of subscribers
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+    - subreddit (str): The name of the subreddit.
+
+    Returns:
+    - int: The number of subscribers or 0 if the subreddit is not found.
     """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyCustomUserAgentNetwork/1.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json()
-        subs = data['data']['subscribers']
-        return subs
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
