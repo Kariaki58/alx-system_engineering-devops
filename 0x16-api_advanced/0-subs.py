7afa0e2e5ce053df8f00eac4771aaa3d21adfd88
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-16 13:17:17 +0100

    reformat output

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,16 @@
 """check how many subsribers"""
 import requests
 
+headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
+
 
 def number_of_subscribers(subreddit):
     """number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subs = data['data']['subscribers']
         return subs
-    return 0
+    else:
+        return 0
