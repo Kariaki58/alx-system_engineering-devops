e26cc266b98e5a913a82403dbdf7b1960cfa3120
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-16 13:09:10 +0100

    modifeid solution

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,9 @@ def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyBot/1.0'}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subs = data['data']['subscribers']
         return subs
-    elif response.status_code == 404:
-        print(f"Subreddit '{subreddit}' not found.")
-    else:
-        print(f"Error: {response.status_code}")
     return 0
