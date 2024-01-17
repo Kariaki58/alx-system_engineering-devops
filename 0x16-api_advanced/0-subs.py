455efeddc9db36fd206529e1dc88e3f5403ee13c
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-17 08:20:18 +0100

    finding bugs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,12 +18,15 @@ def number_of_subscribers(subreddit):
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyCustomUserAgentNetwork/1.0'}
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    except Exception as err:
+        print(f'Error: {e}')
         return 0
