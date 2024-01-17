cd3af1a190d423bbe028d791c9d1d663ae0b7361
Kariaki58 <programmerjavascript901@gmail.com>
2024-01-17 08:29:39 +0100

    fixed bug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,13 +5,13 @@ import requests
 
 def top_ten(subreddit):
     """number of subscribers"""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'MyCustomUserAgent/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         list_of_posts = data['data']['children']
         for post in list_of_posts:
-            print(post['data']['title'])
+            print(post.get('data').get('title'))
     else:
-        print('None')
+        print(None)
